d6ab1cb293772a0a56d6ffcc720677f4416f669d
rohanvraju <rohanvraju@gmail.com>
2026-09-15 15:39:07 -0400

    Drawing tiled background. Attempting to implement optical illusion. Vertex marking needs work for that

--- digiPond/src/canvasComponent.jsx
+++ digiPond/src/canvasComponent.jsx
@@ -1,6 +1,25 @@
 import React, {useRef, useEffect, useState} from 'react';
 import { useFish, updateFish } from './fishComponent';
 
+const drawGrid = (ctx, canvasWidth, canvasHeight, tileSize) =>{
+    const tileColors = ['#00A2E8', '#99D9EA'];
+    const vertexColors = ['#000000', '#ffffff']
+    for(let y = 0; y < canvasHeight; y += tileSize){
+        for(let x = 0; x < canvasWidth; x += tileSize){
+            const tileX = x / tileSize;
+            const tileY = y / tileSize;
+            
+            ctx.fillStyle = tileColors[(tileX + tileY) % 2];
+            ctx.fillRect(x, y, tileSize, tileSize);
+
+            const vertexOriginX = ((tileX + tileY) % 2 == 0) ? (x + tileSize) - 10 : x;
+            const vertexOriginY = ((tileX + tileY) % 2 == 0) ? (y + tileSize) - 10 : (y + tileSize) - 10;
+            ctx.fillStyle = vertexColors[0];
+            ctx.fillRect(vertexOriginX, vertexOriginY, (10), (10));
+        }
+    }
+}
+
 const CanvasComponent = () =>{
     const canvasRef = useRef(null);
     const [dimensions, setDimensions] = useState({
@@ -35,9 +54,10 @@ const CanvasComponent = () =>{
                 updateFish(fish, deltaTime, dimensions.width, dimensions.height);
             });
 
-            ctx.clearRect(0, 0, dimensions.width, dimensions.height);
-            ctx.fillStyle = 'blue';
-            ctx.fillRect(0, 0, dimensions.width, dimensions.height);
+            // ctx.clearRect(0, 0, dimensions.width, dimensions.height);
+            // ctx.fillStyle = 'blue';
+            // ctx.fillRect(0, 0, dimensions.width, dimensions.height);
+            drawGrid(ctx, dimensions.width, dimensions.height, 40);
             drawFish(ctx);
 
             animationId = requestAnimationFrame(animate);
